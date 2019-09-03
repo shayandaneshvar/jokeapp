c4410ba8dddbb46aae078f443aa98916a2673851
shayandaneshvar1 <daneshvarshayan1@gmail.com>
2019-09-03 20:06:04 +0430

    Added Jar and Launcher

--- src/main/java/ir/shayandaneshvar/jokeapp/JokeappApplication.java
+++ src/main/java/ir/shayandaneshvar/jokeapp/JokeappApplication.java
@@ -4,12 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 @ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JokeappApplication.class, args);
+        System.out.println("++++++++++++++++++++++++++ ||| Locked & Loaded " +
+                "||| ++++++++++++++++++++++++++");
+        System.out.println("                           Default: " +
+                "localhost:8080");
+        System.out.println("-------------------------- ||||||||||||||||||||" +
+                "||| --------------------------");
     }
 
 }
